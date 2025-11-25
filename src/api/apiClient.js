b85df5d5631de9b895cebc20d8751877bbb915d0
saipriyasahoo228 <saipriyasahoo228@gmail.com>
2025-11-25 16:04:52 +0530

    BaseURL Change

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -27,7 +27,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",   // your exact backend
+  // baseURL: "http://127.0.0.1:8000/api/",   // your exact backend
+  baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
